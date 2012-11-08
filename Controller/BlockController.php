c515c4b7d9f6b625576e1b2a31c64b256742c841
Tito Miguel Costa <github@titomiguelcosta.com>
2012-11-08 22:14:21 +0000

    added block service

---
+++ Controller/BlockController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Zorbus\GlossaryBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BlockController extends Controller
+{
+    public function defaultAction($block)
+    {
+        $parameters = json_decode($block->getParameters());
+        $glossary = $this->getDoctrine()->getRepository('ZorbusGlossaryBundle:Glossary')->find($parameters->glossary_id);
+        $entries = array();
+        $letters = array();
+        if ($glossary)
+        {
+            $allEntries = $glossary->getEntries();
+            foreach ($allEntries as $entry)
+            {
+                $hash = md5($entry->getLetter());
+                $entries[$hash][] = $entry;
+                $letters[$hash] = $entry->getLetter();
+            }
+        }
+
+        return $this->render('ZorbusBlockBundle:Block:default.html.twig', array(
+            'block' => $block, 'glossary' => $glossary, 'entries' => $entries, 'letters' => $letters
+        ));
+    }
+    public function allAction($block)
+    {
+        $parameters = json_decode($block->getParameters());
+        $glossary = $this->getDoctrine()->getRepository('ZorbusGlossaryBundle:Glossary')->find($parameters->glossary_id);
+        $entries = array();
+        $letters = array();
+        if ($glossary)
+        {
+            foreach (str_split($glossary->getLetters(), 1) as $letter)
+            {
+                $hash = md5($letter);
+                $letters[$hash] = $letter;
+            }
+
+            $allEntries = $glossary->getEntries();
+            foreach ($allEntries as $entry)
+            {
+                $hash = md5($entry->getLetter());
+                $entries[$hash][] = $entry;
+            }
+        }
+        return $this->render('ZorbusBlockBundle:Block:all.html.twig', array(
+            'block' => $block, 'glossary' => $glossary, 'entries' => $entries, 'letters' => $letters
+        ));
+    }
+}
---
+++ Model/BlockGlossaryConfig.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Zorbus\GlossaryBundle\Model;
+
+use Zorbus\BlockBundle\Entity\Block as BlockEntity;
+use Sonata\AdminBundle\Admin\AdminInterface;
+use Symfony\Component\Form\FormFactory;
+use Zorbus\BlockBundle\Model\BlockConfig;
+
+class BlockGlossaryConfig extends BlockConfig
+{
+
+    public function __construct(AdminInterface $admin, FormFactory $formFactory, $httpKernel)
+    {
+        parent::__construct('zorbus_block.service.glossary', 'Glossary Block', $admin, $formFactory);
+        $this->enabled = true;
+        $this->themes = array(
+            'ZorbusBlockBundle:Glossary:default' => 'Default',
+            'ZorbusBlockBundle:Glossary:all' => 'All caracters'
+            );
+        $this->httpKernel = $httpKernel;
+    }
+
+    public function getFormMapper()
+    {
+        return $this->formMapper
+                        ->add('glossary', 'entity', array(
+                            'class' => 'Zorbus\\GlossaryBundle\\Entity\\Glossary',
+                            'attr' => array('class' => 'span5 select2')
+                        ))
+                        ->add('name', 'text')
+                        ->add('lang', 'text', array('required' => false))
+                        ->add('theme', 'choice', array(
+                            'choices' => $this->getThemes(),
+                            'attr' => array('class' => 'span5 select2')
+                        ))
+                        ->add('cache_ttl', 'integer', array(
+                            'required' => false,
+                            'attr' => array('class' => 'span2')
+                        ))
+                        ->add('enabled', 'checkbox', array('required' => false))
+        ;
+    }
+
+    public function getBlockEntity(array $data, BlockEntity $block = null)
+    {
+        $block = null === $block ? new BlockEntity() : $block;
+
+        $block->setService($this->getService());
+        $block->setCategory('Glossary');
+        $block->setParameters(json_encode(array('glossary_id' => $data['glossary']->getId())));
+        $block->setName($data['name']);
+        $block->setLang($data['lang']);
+        $block->setTheme($data['theme']);
+        $block->setEnabled((boolean) $data['enabled']);
+        $block->setCacheTtl($data['cache_ttl']);
+
+        return $block;
+    }
+
+    public function render(BlockEntity $block, $page = null, $request = null)
+    {
+        if ($block->getService() != $this->getService())
+        {
+            throw new \InvalidArgumentException('Block service not supported');
+        }
+
+        $response = $this->httpKernel->forward($block->getTheme(), array('block' => $block, 'page' => $page, 'request' => $request));
+
+        return $response;
+    }
+
+}
